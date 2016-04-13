f2423d7ed96718d7606425bf93771f93db717bca
Jerry Zhang <zjrstar@gmail.com>
2016-04-13 23:38:36 +0800

    first commit

---
+++ src/main/java/com/interview/algorithm/MainTest.java
@@ -0,0 +1,12 @@
+package com.interview.algorithm;
+
+/**
+ * Created by Jerry on 4/13/16.
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
